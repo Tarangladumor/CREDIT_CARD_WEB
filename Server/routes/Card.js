25c08566d108a07a853a1f1a948d81ffae01a7a6
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2024-07-07 00:59:44 +0530

    comment controller is done

--- Server/routes/Card.js
+++ Server/routes/Card.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import { createRating,getAllRating,getAverageRating } from '../controllers/RatingAndReview.js';
+import { createComment, deleteComment, editComment, getAllComments } from '../controllers/Comment.js';
 
 const router = express.Router()
 
 
 //  Rating And Review Routes
-router.post("/createRating",createRating);
-router.post('/getAverageRating', getAverageRating);
-router.get('/getAllRating',getAllRating);
+router.post("/create-rating",createRating);
+router.post('/get-average-rating', getAverageRating);
+router.get('/get-all-rating',getAllRating);
+
+//comments
+router.post("/create-comment",createComment);
+router.put("/edit-comment",editComment);
+router.delete("/delete-comment",deleteComment);
+router.get('/comments', getAllComments);
 
 
 
