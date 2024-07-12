94fbfae47f11ff988fa15bc4862d03948e0b4447
Shabbir Saiyad <shabbirsaiyad83@gmail.com>
2024-07-12 09:15:12 +0530

    completed additional benefits controller

--- Server/routes/Card.js
+++ Server/routes/Card.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { createRating,getAllRating,getAverageRating } from '../controllers/RatingAndReview.js';
 import { createComment, deleteComment, editComment, getAllComments } from '../controllers/Comment.js';
+import { addAdditionalBenefits, editAdditionalBenefits,getAllAdditionalBenefits ,deleteAdditionalBenefits} from '../controllers/AdditionalBenefit.js';
 
 const router = express.Router()
 
@@ -15,6 +16,12 @@ router.post("/create-comment",createComment);
 router.put("/edit-comment",editComment);
 router.delete("/delete-comment",deleteComment);
 router.get('/comments', getAllComments);
+
+//Additional Benfits
+router.post('/add-additional-benefit', addAdditionalBenefits);
+router.put('/edit-additional-benefit', editAdditionalBenefits); 
+router.get('/get-all-additional-benefits',getAllAdditionalBenefits);
+router.delete('/delete-additional-benefit', deleteAdditionalBenefits);
 
 
 
