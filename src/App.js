e758a70f57e792a5defdb5577197e717c5d80899
Parth4120 <patelparth4120@gmail.com>
2024-07-21 17:06:56 +0530

    comparecard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
 import Page_2 from './Pages/Page_2';
+import CompareCard from './Pages/CompareCard';
 
 function App() {
   return (
    <div>
     {/* <Home/> */}
-    <Page_2/>
+    {/* <Page_2/> */}
+    <CompareCard/>
    </div>
   );
 }
